a28d43b8722020091e21f410ab8bd1a97a141589
Jackson Graves <jackson@unconventionalcode.com>
2024-01-11 15:06:01 -0600

    fix: migrate bit-buffer to TS

--- lib/core/bit-buffer.ts
+++ lib/core/bit-buffer.ts
@@ -1,25 +1,27 @@
-function BitBuffer() {
-	this.buffer = [];
-	this.length = 0;
-}
+export default class BitBuffer {
+	length: number;
+	buffer: number[];
+	constructor() {
+		this.buffer = [];
+		this.length = 0;
+	}
 
-BitBuffer.prototype = {
-	get: function (index) {
+	get(index: number) {
 		const bufIndex = Math.floor(index / 8);
 		return ((this.buffer[bufIndex] >>> (7 - (index % 8))) & 1) === 1;
-	},
+	}
 
-	put: function (num, length) {
+	put(num: number, length: number) {
 		for (let i = 0; i < length; i++) {
-			this.putBit(((num >>> (length - i - 1)) & 1) === 1);
+			this.putBit(((num >>> (length - i - 1)) & 1) === 1 ? 1 : 0);
 		}
-	},
+	}
 
-	getLengthInBits: function () {
+	getLengthInBits() {
 		return this.length;
-	},
+	}
 
-	putBit: function (bit) {
+	putBit(bit: number) {
 		const bufIndex = Math.floor(this.length / 8);
 		if (this.buffer.length <= bufIndex) {
 			this.buffer.push(0);
@@ -30,7 +32,5 @@ BitBuffer.prototype = {
 		}
 
 		this.length++;
-	},
-};
-
-module.exports = BitBuffer;
+	}
+}
