22d233c13839634df15a6545802320e6aa68eea7
Jackson Graves <jackson@unconventionalcode.com>
2024-01-11 14:59:55 -0600

    fix: migrate structured-append-data to TS

---
+++ lib/core/structured-append-data.ts
@@ -0,0 +1,25 @@
+import BitBuffer from './bit-buffer';
+import * as Mode from './mode';
+
+export default class StructuredAppendData {
+	mode: Mode.Mode;
+	data: { position: number; total: number; parity: number };
+	constructor(data: { position: number; total: number; parity: number }) {
+		this.mode = Mode.STRUCTURED_APPEND;
+		this.data = data;
+	}
+
+	getBitsLength() {
+		return 16;
+	}
+
+	getLength() {
+		return 0;
+	}
+
+	write(bitBuffer: BitBuffer) {
+		bitBuffer.put(this.data.position, 4);
+		bitBuffer.put(this.data.total, 4);
+		bitBuffer.put(this.data.parity, 8);
+	}
+}
