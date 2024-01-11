bf940e8cf91bd0c6e39eaba9d3418bd778eb63ec
Jackson Graves <jackson@unconventionalcode.com>
2024-01-11 03:14:11 -0600

    fix: migrate test/unit/core/finder-pattern to jest

---
+++ test/unit/core/finder-pattern.test.ts
@@ -0,0 +1,9 @@
+import pattern from '../../../lib/core/finder-pattern';
+
+describe('Finder pattern', () => {
+	it('should always return 3 pattern positions', () => {
+		for (let i = 1; i <= 40; i++) {
+			expect(pattern.getPositions(i).length).toEqual(3);
+		}
+	});
+});
