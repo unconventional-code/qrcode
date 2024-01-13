d91f07f74f9083e71fd8fcdde99f2516e7d68e66
Jackson Graves <jackson@unconventionalcode.com>
2024-01-13 06:19:44 -0600

    fix: add types to error-correction-level test

--- test/unit/core/error-correction-level.test.ts
+++ test/unit/core/error-correction-level.test.ts
@@ -1,11 +1,9 @@
-// @ts-nocheck
 import * as ECLevel from '../../../lib/core/error-correction-level';
-import { QRCodeErrorCorrectionLevel } from '../../../types';
 
 const EC_LEVELS = [ECLevel.L, ECLevel.M, ECLevel.Q, ECLevel.H];
 
 describe('Error level from input value', () => {
-	const values: QRCodeErrorCorrectionLevel[][] = [
+	const values: [ECLevel.QRCodeErrorCorrectionLevel, ECLevel.QRCodeErrorCorrectionLevel][] = [
 		['L', 'low'],
 		['M', 'medium'],
 		['Q', 'quartile'],
@@ -16,7 +14,9 @@ describe('Error level from input value', () => {
 		for (let l = 0; l < values.length; l++) {
 			for (let i = 0; i < values[l].length; i++) {
 				expect(ECLevel.from(values[l][i])).toBe(EC_LEVELS[l]);
-				expect(ECLevel.from(values[l][i].toUpperCase())).toBe(EC_LEVELS[l]);
+				expect(ECLevel.from(values[l][i].toUpperCase() as ECLevel.QRCodeErrorCorrectionLevel)).toBe(
+					EC_LEVELS[l]
+				);
 			}
 		}
 	});
@@ -28,6 +28,6 @@ describe('Error level from input value', () => {
 		expect(ECLevel.from(undefined, 'M')).toBe(ECLevel.M);
 	});
 	it('should return default level if value is invalid', () => {
-		expect(ECLevel.from('', 'Q')).toBe(ECLevel.Q);
+		expect(ECLevel.from('' as ECLevel.QRCodeErrorCorrectionLevel, 'Q')).toBe(ECLevel.Q);
 	});
 });
